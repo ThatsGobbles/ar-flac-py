bb129f003bf6af76c42e8236a383ba1a473cec63
Mark LeMoine <thatsgobbles@gmail.com>
2018-07-31 14:40:03 -0700

    Created calc_ar_disc_ids

--- ar_flac.py
+++ ar_flac.py
@@ -7,6 +7,11 @@ import math
 CDDA_BITS_PER_FRAME = 588
 CDDA_FRAMES_PER_SEC = 75
 
+class ARDiscIds(tp.NamedTuple):
+    disc_id_1: int
+    disc_id_2: int
+    cddb_disc_id: int
+
 def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='Validate FLAC files against the online AccurateRip database.')
 
@@ -36,6 +41,46 @@ def yield_track_offsets(flac_files: tp.Iterable[pl.Path]) -> tp.Generator[int, N
     # Yield the offset after the final track.
     yield offset
 
+def calc_ar_disc_ids(track_offsets: tp.Iterable[int]) -> ARDiscIds:
+    disc_id_1 = 0
+    disc_id_2 = 0
+    cddb_disc_id = 0
+
+    first_offset = None
+
+    for track_num, track_offset in enumerate(track_offsets, start=0):
+        if first_offset is None:
+            first_offset = track_offset
+
+        disc_id_1 += track_offset
+        disc_id_2 += (track_offset if bool(track_offset) else 1) * (track_num + 1)
+
+        if track_num < len(track_offsets) - 1:
+            cddb_disc_id += sum_digits(int(track_offset // CDDA_FRAMES_PER_SEC) + 2)
+
+    if first_offset is not None:
+        cddb_disc_id = (
+            ((cddb_disc_id % 255) << 24)
+            + (((track_offset // CDDA_FRAMES_PER_SEC) - (first_offset // 75)) << 8)
+            + (len(track_offsets) - 1)
+        )
+
+    disc_id_1 &= 0xFFFFFFFF
+    disc_id_2 &= 0xFFFFFFFF
+    cddb_disc_id &= 0xFFFFFFFF
+
+    return ARDiscIds(disc_id_1, disc_id_2, cddb_disc_id)
+
+def sum_digits(n: int) -> int:
+    '''Sums the digits in an integer.'''
+    r = 0
+
+    while n > 0:
+        r += n % 10
+        n //= 10
+
+    return r
+
 if __name__ == '__main__':
     parser = get_arg_parser()
     parsed_args = parser.parse_args()
@@ -47,4 +92,8 @@ if __name__ == '__main__':
     for f in flac_files:
         print(f'Found file: {f.name}')
 
-    print(list(yield_track_offsets(flac_files)))
+    track_offsets = list(yield_track_offsets(flac_files))
+    print(track_offsets)
+
+    ar_disc_ids = calc_ar_disc_ids(track_offsets)
+    print(ar_disc_ids)
